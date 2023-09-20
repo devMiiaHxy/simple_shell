c724cffa99aa03a9d83e7af479e703a3fb3cabc7
root <root@DESKTOP-EEU6TB3.localdomain>
2023-09-20 23:08:23 +0100

    all files

--- work.c
+++ work.c
@@ -7,6 +7,7 @@
 
 int main(int argc, char *argv[])
 {
+<<<<<<< HEAD
 	int i;
 	int *waiter;
 	pid_t pid;
@@ -52,4 +53,43 @@ int main(int argc, char *argv[])
 	}
 	}
 	return (0);
+=======
+        int i, *waiter;
+        pid_t pid;
+        char* content, **arg;
+        size_t x;
+        (void) argv;
+
+        while(1)
+        {
+
+        waiter = 0;
+        x = 0;
+        getline(&content, &x, stdin);
+        arg = brkstr(content);
+
+        pid = fork();
+        if (pid == 0)
+        {
+                for (i = 0; i < argc; i++)
+                {
+                        if (execve(arg[0], arg, NULL) == -1)
+                        {
+                                perror("something went wrong");
+                        }
+                        else
+                        {
+                                exit(EXIT_FAILURE);
+                        }
+                }
+        }
+
+        else
+        {
+                wait(waiter);
+                printf("done\n");
+        }
+        }
+        return (0);
+>>>>>>> origin/main
 }
